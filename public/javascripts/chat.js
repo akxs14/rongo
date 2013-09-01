671d4d1bb63fdb2f2f91ba2f1e5fd1af9b11a353
akxs14 <angelos.kapsimanis@madvertise.com>
2013-09-01 22:47:53 +0200

    cleaned index, added pure.css

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,24 +1,24 @@
-        var sockjs_url = '/chat';
-        var sockjs = new SockJS(sockjs_url);
+var sockjs_url = '/chat';
+var sockjs = new SockJS(sockjs_url);
 
-        var userid = 'guest' + new Date().getSeconds();
-        var div  = $('#first div');
-        var inp  = $('#first input');
-        var form = $('#first form');
+var userid = 'guest' + new Date().getSeconds();
+var div  = $('#first div');
+var inp  = $('#first input');
+var form = $('#first form');
 
-        var print = function(message){
-            div.append($("<code>").text(message));
-            div.append($("<br>"));
-            div.scrollTop(div.scrollTop()+10000);
-        }
+var print = function(message){
+  div.append($("<code>").text(message));
+  div.append($("<br>"));
+  div.scrollTop(div.scrollTop()+10000);
+}
 
-        sockjs.onopen    = function()  {print('Connected.');};
-        sockjs.onmessage = function(e) {print(e.data);};
-        sockjs.onclose   = function()  {print('Closing Connection.');};
+sockjs.onopen    = function()  {print('Connected.');};
+sockjs.onmessage = function(e) {print(e.data);};
+sockjs.onclose   = function()  {print('Closing Connection.');};
 
-        form.submit(function() {
-            print('Sending to server...');
-            sockjs.send(userid + ': ' + inp.val());
-            inp.val('');
-            return false;
-        });
+form.submit(function() {
+  print('Sending to server...');
+  sockjs.send(userid + ': ' + inp.val());
+  inp.val('');
+  return false;
+});
